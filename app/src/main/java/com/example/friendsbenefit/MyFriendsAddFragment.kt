9342df779f18d81d6aab6f90186d683100ebccf1
Aurellie Azzahra <aurellieazzahra07@gmail.com>
2024-03-04 00:04:47 +0700

    feat: add friend's data from database

--- app/src/main/java/com/example/friendsbenefit/MyFriendsAddFragment.kt
+++ app/src/main/java/com/example/friendsbenefit/MyFriendsAddFragment.kt
@@ -1,11 +1,18 @@
 package com.example.friendsbenefit
 
+import MyFriend
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.friendsbenefit.databinding.AddFriendsBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MyFriendsAddFragment : Fragment() {
 
@@ -16,6 +23,13 @@ class MyFriendsAddFragment : Fragment() {
             return MyFriendsAddFragment()
         }
     }
+    private var namaInput : String = ""
+    private var emailInput : String = ""
+    private var telpInput : String = ""
+    private var alamatInput : String = ""
+    private var genderInput : String = ""
+    private var db: AppDatabase? = null
+    private var myFriendDao: MyFriendDao? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,12 +42,64 @@ class MyFriendsAddFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initLocalDB()
         initView()
+    }
+    private fun initLocalDB() {
+        db = AppDatabase.getAppDataBase(requireActivity())
+        myFriendDao = db?.myFriendDao()
     }
 
     private fun initView() {
         binding.btnSave.setOnClickListener {
             (activity as MainActivity).tampilMyFriendsFragment()
+            setDataSpinnerGener()
         }
     }
+    private fun setDataSpinnerGener() {
+        val adapter = ArrayAdapter.createFromResource(activity!!,
+            R.array.gender_list,
+            android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_drop
+                down_item)
+        spinnerGender.adapter = adapter
+    }
+    private fun validasiInput() {
+        private val edtName: TextView = itemView.findViewById(R.id.edtName)
+
+        namaInput = edtName.text.toString()
+        emailInput = edtEmail.text.toString()
+        telpInput = edtTelp.text.toString()
+        alamatInput = edtAddress.text.toString()
+        genderInput = spinnerGender.selectedItem.toString()
+        when{
+            namaInput.isEmpty() -> edtName.error = "Nama tidak boleh kosong"
+                genderInput.equals("Pilih kelamin") > tampilToast("Kelamin harus dipilih")
+            emailInput.isEmpty() -> edtEmail.error = "Email tidak boleh kosong"
+                telpInput.isEmpty() -> edtTelp.error = "Telp tidak boleh kosong"
+                alamatInput.isEmpty() -> edtAddress.error = "Alamat tidak boleh kosong"
+
+            else -> {
+                val teman = MyFriend(nama = namaInput, kelamin =
+                genderInput, email = emailInput, telp = telpInput, alamat =
+                alamatInput)
+                tambahDataTeman(teman)
+            }
+        }
+    }
+    private fun tambahDataTeman(teman: MyFriend) : Job {
+        return GlobalScope.launch {
+            myFriendDao?.tambahTeman(teman)
+            (activity as MainActivity).tampilMyFriendsFragment()
+        }
+    }
+    private fun tampilToast(message: String) {
+        Toast.makeText(requireActivity(), message,
+            Toast.LENGTH_SHORT).show()
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        this.clearFindViewByIdCache()
+    }
+
 }
--- app/src/main/java/com/example/friendsbenefit/MyFriendsFragment.kt
+++ app/src/main/java/com/example/friendsbenefit/MyFriendsFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.friendsbenefit.databinding.FriendsFragmentBinding
 
@@ -13,24 +15,18 @@ class MyFriendsFragment : Fragment() {
 
     private lateinit var myFriendList : MutableList<MyFriend>
     private lateinit var binding: FriendsFragmentBinding
+    private var db: AppDatabase? = null
+    private var myFriendDao: MyFriendDao? = null
 
-    private fun simulationMyFriendsData() {
-        myFriendList = ArrayList()
-        myFriendList.add(MyFriend("Relliye", "Perempuan",
-            "relliye@gmail.com", "082140333911", "Tuban"))
-        myFriendList.add(MyFriend("Ujang", "Laki-laki",
-            "ujang@gmail.com", "081213416171", "Malang"))
-        myFriendList.add(MyFriend("Soepomo", "Laki-laki",
-            "soepomo@gmail.com", "08941796577", "Malang"))
-    }
-
-    private fun showMyFriends(){
-        binding.listMyFriends.layoutManager = LinearLayoutManager(activity)
-        binding.listMyFriends.adapter = MyFriendAdapter(requireActivity(),
-            myFriendList as ArrayList<MyFriend>
-        )
-    }
-
+//    private fun simulationMyFriendsData() {
+//        myFriendList = ArrayList()
+//        myFriendList.add(MyFriend("Relliye", "Perempuan",
+//            "relliye@gmail.com", "082140333911", "Tuban"))
+//        myFriendList.add(MyFriend("Ujang", "Laki-laki",
+//            "ujang@gmail.com", "081213416171", "Malang"))
+//        myFriendList.add(MyFriend("Soepomo", "Laki-laki",
+//            "soepomo@gmail.com", "08941796577", "Malang"))
+//    }
 
     companion object {
         fun newInstance(): MyFriendsFragment {
@@ -49,14 +45,46 @@ class MyFriendsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initLocalDB()
         initView()
+    }
+    private fun initLocalDB() {
+        db = AppDatabase.getAppDataBase(requireActivity())
+        myFriendDao = db?.myFriendDao()
     }
 
     private fun initView() {
         binding.fabAddFriend.setOnClickListener {
             (activity as MainActivity).tampilMyFriendsAddFragment()
         }
-        simulationMyFriendsData()
-        showMyFriends()
+        ambilDataTeman()
     }
+    private fun ambilDataTeman() {
+        myFriendList = ArrayList()
+        myFriendDao?.ambilSemuaTeman()?.observe(viewLifecycleOwner, Observer { r ->
+            myFriendList = r as MutableList<MyFriend>
+            when {
+                myFriendList?.size == 0 -> tampilToast("Belum ada data teman")
+            }
+            else -> {
+            showMyFriends()
+
+        }
+        }
+        )
+    }
+    private fun tampilToast(message: String) {
+        Toast.makeText(requireActivity(), message, Toast.LENGTH_SHORT).show()
+    }
+    private fun showMyFriends(){
+        binding.listMyFriends.layoutManager = LinearLayoutManager(activity)
+        binding.listMyFriends.adapter = MyFriendAdapter(requireActivity(),
+            myFriendList as ArrayList<MyFriend>
+        )
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        this.clearFindViewByIdCache()
+    }
+
 }
