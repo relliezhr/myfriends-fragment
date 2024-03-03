8ba8b95c12210221900b6ad33480129d2ca0ea7c
Aurellie Azzahra <aurellieazzahra07@gmail.com>
2024-03-03 23:41:17 +0700

    feat: create data access object

---
+++ app/src/main/java/com/example/friendsbenefit/MyFriendDao.kt
@@ -0,0 +1,17 @@
+package com.example.friendsbenefit
+
+import MyFriend
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+import androidx.lifecycle.LiveData
+
+@Dao
+interface MyFriendDao{
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun tambahTeman(friend: MyFriend)
+
+    @Query("SELECT * FROM MyFriend")
+    fun ambilSemuaTeman(): LiveData<List<MyFriend>>
+}
