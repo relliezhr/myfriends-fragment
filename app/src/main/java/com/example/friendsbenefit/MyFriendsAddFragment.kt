e96653530a1fcb0a2b3330c1b6dd49bdc76beed5
Aurellie Azzahra <aurellieazzahra07@gmail.com>
2024-03-04 13:32:04 +0700

    fix: update input validation

--- app/src/main/java/com/example/friendsbenefit/MyFriendsAddFragment.kt
+++ app/src/main/java/com/example/friendsbenefit/MyFriendsAddFragment.kt
@@ -6,30 +6,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.friendsbenefit.databinding.AddFriendsBinding
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class MyFriendsAddFragment : Fragment() {
 
     private lateinit var binding: AddFriendsBinding
+    private lateinit var namaInput: String
+    private lateinit var emailInput: String
+    private lateinit var telpInput: String
+    private lateinit var alamatInput: String
+    private lateinit var genderInput: String
+    private var db: AppDatabase? = null
+    private var myFriendDao: MyFriendDao? = null
 
     companion object {
         fun newInstance(): MyFriendsAddFragment {
             return MyFriendsAddFragment()
         }
     }
-    private var namaInput : String = ""
-    private var emailInput : String = ""
-    private var telpInput : String = ""
-    private var alamatInput : String = ""
-    private var genderInput : String = ""
-    private var db: AppDatabase? = null
-    private var myFriendDao: MyFriendDao? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,61 +45,60 @@ class MyFriendsAddFragment : Fragment() {
         initLocalDB()
         initView()
     }
+
     private fun initLocalDB() {
-        db = AppDatabase.getAppDataBase(requireActivity())
+        db = AppDatabase.getAppDatabase(requireActivity())
         myFriendDao = db?.myFriendDao()
     }
 
     private fun initView() {
         binding.btnSave.setOnClickListener {
-            (activity as MainActivity).tampilMyFriendsFragment()
-            setDataSpinnerGener()
+            validasiInput()
         }
+        setDataSpinnerGender()
     }
-    private fun setDataSpinnerGener() {
-        val adapter = ArrayAdapter.createFromResource(activity!!,
+
+    private fun setDataSpinnerGender() {
+        val adapter = ArrayAdapter.createFromResource(
+            requireActivity(),
             R.array.gender_list,
-            android.R.layout.simple_spinner_item)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_drop
-                down_item)
-        spinnerGender.adapter = adapter
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerGender.adapter = adapter
     }
+
     private fun validasiInput() {
-        private val edtName: TextView = itemView.findViewById(R.id.edtName)
+        namaInput = binding.edtName.text.toString()
+        emailInput = binding.edtEmail.text.toString()
+        telpInput = binding.edtTelp.text.toString()
+        alamatInput = binding.edtAddress.text.toString()
+        genderInput = binding.spinnerGender.selectedItem.toString()
 
-        namaInput = edtName.text.toString()
-        emailInput = edtEmail.text.toString()
-        telpInput = edtTelp.text.toString()
-        alamatInput = edtAddress.text.toString()
-        genderInput = spinnerGender.selectedItem.toString()
-        when{
-            namaInput.isEmpty() -> edtName.error = "Nama tidak boleh kosong"
-                genderInput.equals("Pilih kelamin") > tampilToast("Kelamin harus dipilih")
-            emailInput.isEmpty() -> edtEmail.error = "Email tidak boleh kosong"
-                telpInput.isEmpty() -> edtTelp.error = "Telp tidak boleh kosong"
-                alamatInput.isEmpty() -> edtAddress.error = "Alamat tidak boleh kosong"
-
+        when {
+            namaInput.isEmpty() -> binding.edtName.error = "Nama tidak boleh kosong"
+            genderInput == "Pilih Jenis kelamin" -> tampilToast("Jenis Kelamin harus dipilih")
+            emailInput.isEmpty() -> binding.edtEmail.error = "Email tidak boleh kosong"
+            telpInput.isEmpty() -> binding.edtTelp.error = "Telp tidak boleh kosong"
+            alamatInput.isEmpty() -> binding.edtAddress.error = "Alamat tidak boleh kosong"
             else -> {
-                val teman = MyFriend(nama = namaInput, kelamin =
-                genderInput, email = emailInput, telp = telpInput, alamat =
-                alamatInput)
+                val teman = MyFriend(null, namaInput, genderInput, emailInput, telpInput, alamatInput)
                 tambahDataTeman(teman)
             }
         }
     }
-    private fun tambahDataTeman(teman: MyFriend) : Job {
-        return GlobalScope.launch {
+
+    private fun tambahDataTeman(teman: MyFriend): Job {
+        return CoroutineScope(Dispatchers.IO).launch {
             myFriendDao?.tambahTeman(teman)
-            (activity as MainActivity).tampilMyFriendsFragment()
+            requireActivity().runOnUiThread {
+                (activity as MainActivity).tampilMyFriendsFragment()
+            }
         }
     }
+
     private fun tampilToast(message: String) {
-        Toast.makeText(requireActivity(), message,
-            Toast.LENGTH_SHORT).show()
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        this.clearFindViewByIdCache()
+        Toast.makeText(requireActivity(), message, Toast.LENGTH_SHORT).show()
     }
 
 }
